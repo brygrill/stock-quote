f5295248fd716a08ac925856574ff5b7f37234cc
Bryan Grill <grillb@gmail.com>
2017-11-18 03:46:51 -0500

    add request to iex symbols

--- docker/index.js
+++ docker/index.js
@@ -1,7 +1,6 @@
+/* eslint-disable no-console */
 require('dotenv').config();
-const csv = require('csvtojson');
-const Client = require('ftp');
-const fs = require('fs');
+const axios = require('axios');
 const algoliasearch = require('algoliasearch');
 
 // init algolia
@@ -11,138 +10,57 @@ const CLIENT = algoliasearch(
 );
 const INDEX = CLIENT.initIndex(process.env.ALGOLIA_INDEX);
 
-// constants for Nasdaq ftp data
-const NAS_HOST = 'ftp.nasdaqtrader.com';
-const NAS_DIR = 'SymbolDirectory';
-const NAS = 'nasdaqlisted';
-const OTH = 'otherlisted';
-
-// constants for saving data to file
-const DATA = []; // in memory array to collect data during conversion
-const DATA_DIR = 'data';
-const OUT = `./${DATA_DIR}/listed.json`;
-
-// Get data from ftp and save to local file
-const fetchFTP = filename => {
-  return new Promise((resolve, reject) => {
-    const c = new Client();
-    c.on('ready', () => {
-      c.get(`${NAS_DIR}/${filename}.txt`, (err, stream) => {
-        if (err) {
-          reject(err);
-        }
-        stream.once('close', () => {
-          c.end();
-          resolve();
-        });
-        stream.pipe(fs.createWriteStream(`./${DATA_DIR}/${filename}.txt`));
-      });
+// fetch symbols from iex
+const iex = () => {
+  return axios
+    .get('https://api.iextrading.com/1.0/ref-data/symbols')
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => {
+      return err;
     });
-    c.connect({ host: NAS_HOST });
-  });
 };
 
-const cleanTextData = filename => {
-  const file = `./${DATA_DIR}/${filename}.txt`;
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      const result = data.replace(/ACT Symbol/g, 'Symbol');
-
-      fs.writeFile(file, result, 'utf8', error => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-  });
-};
-
-// convert txt file to json
-const csvToJson = filename => {
-  const converter = csv({
-    delimiter: ['|'],
-  });
-
-  return new Promise((resolve, reject) => {
-    converter
-      .fromFile(`./${DATA_DIR}/${filename}.txt`)
-      .on('json', jsonObj => {
-        DATA.push(jsonObj);
-      })
-      .on('done', err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-  });
-};
-
-const cleanJsonData = () => {
+// rm test symbols
+const formatData = data => {
   return new Promise((resolve, reject) => {
     try {
-      // rm this entry since its not a symbol
-      DATA.map((item, index) => {
-        if (item.Symbol.startsWith('File Creation Time')) {
-          DATA.splice(index, 1);
+      data.map((item, index) => {
+        if (item.name === 'NASDAQ TEST STOCK') {
+          data.splice(index, 1);
         }
+        return item;
       });
-      // sort a-z
-      DATA.sort((a, b) => {
-        const symbolA = a.Symbol;
-        const symbolB = b.Symbol;
-        if (symbolA < symbolB) {
-          return -1;
-        }
-        if (symbolA > symbolB) {
-          return 1;
-        }
-        return 0;
-      });
-      resolve();
+      resolve(data);
     } catch (err) {
       reject(err);
     }
   });
 };
 
-const saveJsonToDisk = out => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(out, JSON.stringify(DATA, null, 2), 'utf-8', err => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
-
+// clear current index
 const clearIndex = () => {
   return new Promise((resolve, reject) => {
     INDEX.clearIndex(err => {
       if (err) {
         reject();
       } else {
+        console.info('Cleared Index');
         resolve();
       }
     });
   });
 };
 
-const updateIndex = data => {
-  const objects = require(data); // eslint-disable-line global-require
+// populate index
+const updateIndex = objects => {
   return new Promise((resolve, reject) => {
     INDEX.addObjects(objects, (err, content) => {
       if (err) {
         reject(err);
       } else {
+        console.info(`Added ${content.length} records to Index.`);
         resolve(content);
       }
     });
@@ -150,20 +68,13 @@ const updateIndex = data => {
 };
 
 // fetch data
-// convert it
-// save to json
 // push to algolia
 const processData = async () => {
   try {
-    await fetchFTP(NAS);
-    await fetchFTP(OTH);
-    await cleanTextData(OTH);
-    await csvToJson(NAS);
-    await csvToJson(OTH);
-    await cleanJsonData();
-    await saveJsonToDisk(OUT);
+    const data = await iex();
+    const symbols = await formatData(data);
     await clearIndex();
-    await updateIndex(OUT);
+    await updateIndex(symbols);
   } catch (err) {
     console.error(err);
   }
