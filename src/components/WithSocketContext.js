774890d805fe58ca843f58d9abac646d21453b7a
Bryan Grill <grillb@gmail.com>
2018-12-07 06:48:01 -0500

    connect socket

--- src/components/WithSocketContext.js
+++ src/components/WithSocketContext.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import io from 'socket.io-client';
+
+const socket = io('https://ws-api.iextrading.com/1.0/tops');
+
+const benchmarks = ['SPY', 'QQQ'];
 
 export const SocketContext = React.createContext();
 
@@ -15,12 +20,34 @@ export const SocketProvider = props => {
 
   const [quote, setQuote] = useState(null);
 
-  const openSocket = () => {
-    console.log('open socket');
+  const [benchmarkQuotes, setbenchmarkQuotes] = useState([]);
+
+  const ws = () => {
+    try {
+      socket.on('connect', () => {
+        setSession({ loading: false, connected: true, error: null });
+        socket.emit('subscribe', 'firehose');
+      });
+
+      socket.on('disconnect', () => {
+        setSession({ loading: false, connected: false, error: null });
+      });
+
+      socket.on('message', message => {
+        const parse = JSON.parse(message);
+        if (_.includes(benchmarks, parse.symbol)) {
+          console.log(parse);
+          // TODO: dont lose previous state
+          setbenchmarkQuotes([...benchmarkQuotes, parse]);
+        }
+      });
+    } catch (error) {
+      setSession({ loading: false, connected: false, error });
+    }
   };
 
   useEffect(() => {
-    openSocket();
+    ws();
   }, []);
 
   return (
@@ -29,6 +56,7 @@ export const SocketProvider = props => {
         session,
         symbol,
         quote,
+        benchmarkQuotes,
         setSymbol,
         ...props,
       }}
