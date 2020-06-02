49e9bef884540d07cda5be9be47ba56fab55c610
Bryan Grill <grillb@gmail.com>
2020-06-02 17:39:57 -0400

    pass symbol into fetch function

--- app/src/components/QueryQuote.tsx
+++ app/src/components/QueryQuote.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-const endpoint = `https://sandbox.iexapis.com/stable/stock/AAPL/quote?token=${process.env.REACT_APP_IEX}`;
-// async function fetchQuote() {
-//   return (await fetch(
-//     ,
-//   )).json();
-// }
+const endpoint = (symbol: string) =>
+  `https://sandbox.iexapis.com/stable/stock/${symbol}/quote?token=${process.env.REACT_APP_IEX}`;
 
-const fetchQuote = async (symbol: string) => {
+const fetchQuote = async (key: string, { symbol }: { symbol: string }) => {
   console.log(symbol);
-  const { data } = await axios.get(endpoint);
+  const { data } = await axios.get(endpoint(symbol));
   return data;
 };
 
 const Quote = ({ symbol }: { symbol: string }) => {
-  const { data } = useQuery('quote', fetchQuote, { suspense: true });
+  const { data } = useQuery(['quote', { symbol }], fetchQuote, {
+    suspense: true,
+  });
   console.log(data);
   return <div>{symbol}</div>;
 };
