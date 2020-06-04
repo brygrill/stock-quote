cc87dd0cdd6bba3b35e0085fd803f6481ecdd906
Bryan Grill <grillb@gmail.com>
2020-06-04 14:28:21 -0400

    add highlights placeholder with memo

---
+++ app/src/components/Highlights.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Highlights = () => {
+  console.log('render');
+  return (
+    <div>
+      SP500, Nasdaq, Bitcoin, etc.
+    </div>
+  );
+};
+
+export default Highlights;
--- app/src/routes/Home.tsx
+++ app/src/routes/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Highlights from '../components/Highlights';
+const MemoHighlights = React.memo(Highlights);
+
 const Home = () => {
   return (
     <div>
+      <MemoHighlights />
       <Link to="/quote/aapl">Apple</Link>
     </div>
   );
