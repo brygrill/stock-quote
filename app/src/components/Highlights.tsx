fc5e7990164f73755ced0a4bbad852dbd7ca9a7b
Bryan Grill <grillb@gmail.com>
2020-06-05 17:54:43 -0400

    route to quote page on select

--- app/src/components/Highlights.tsx
+++ app/src/components/Highlights.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Highlights = () => {
-  console.log('render');
   return (
     <div>
       SP500, Nasdaq, Bitcoin, etc.
--- app/src/components/QuerySearch.tsx
+++ app/src/components/QuerySearch.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import { TextField } from '@material-ui/core';
+import { TextField, CircularProgress } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 
 import { iex } from '../config';
@@ -18,11 +19,14 @@ const fetchSearch = async (
 };
 
 const QuerySearch = () => {
+  // state
   const [value, setValue] = React.useState<string | null>(null);
   const [inputValue, setInputValue] = React.useState<string | undefined>('');
   const [options, setOptions] = React.useState<string[]>([]);
 
-  const { status, error, data } = useQuery(
+  // hooks
+  const navigateTo = useNavigate();
+  const { isFetching, error, data } = useQuery(
     ['search', { fragment: inputValue }],
     fetchSearch,
   );
@@ -35,20 +39,29 @@ const QuerySearch = () => {
   // select from autocomplete options
   const handleValueSelect = (event: any, newInputValue: string | null) => {
     console.log('route to new page');
-    console.log(newInputValue)
+    console.log(newInputValue);
+    navigateTo(`/quote/${newInputValue}`);
+
     setValue(newInputValue);
   };
 
   React.useEffect(() => {
     if (data) {
       const d = data.map((option: { symbol: string }) => option.symbol);
-      setOptions(d)
+      setOptions(d);
     }
   }, [data]);
+
+  React.useEffect(() => {
+    if (error) {
+      console.error(error);
+    }
+  }, [error]);
 
   return (
     <div>
       <Autocomplete
+        loading={isFetching}
         freeSolo
         value={value}
         onChange={handleValueSelect}
@@ -62,6 +75,17 @@ const QuerySearch = () => {
             label="Search for a stock"
             margin="normal"
             variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <React.Fragment>
+                  {isFetching ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </React.Fragment>
+              ),
+            }}
           />
         )}
       />
