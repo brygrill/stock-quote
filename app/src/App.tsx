61c7093b67035e69060a7ea302f35050e11a2d32
Bryan Grill <grillb@gmail.com>
2020-06-05 08:18:40 -0400

    add dev tools

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactQueryDevtools } from 'react-query-devtools';
+
 import ErrorBoundary from './components/ErrorBoundary';
 import Layout from './layouts';
 import Router from './routes';
 
 const App = () => (
-  <ErrorBoundary render={() => <div>The entire app broke!</div>}>
-    <BrowserRouter>
-      <React.Suspense fallback={<div/>}>
-        <Layout>
-          <React.Suspense fallback={<div>Loading app routes...</div>}>
-            <Router />
-          </React.Suspense>
-        </Layout>
-      </React.Suspense>
-    </BrowserRouter>
-  </ErrorBoundary>
+  <>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <ErrorBoundary render={() => <div>The entire app broke!</div>}>
+      <BrowserRouter>
+        <React.Suspense fallback={<div />}>
+          <Layout>
+            <React.Suspense fallback={<div>Loading app routes...</div>}>
+              <Router />
+            </React.Suspense>
+          </Layout>
+        </React.Suspense>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </>
 );
 
 export default App;
