60c96267d5533e619f0fa40d527407cf0927b716
Bryan Grill <grillb@gmail.com>
2020-06-07 08:00:47 -0400

    add attribution to footer

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import {
-  AppBar,
-  Typography,
-  makeStyles,
-  createStyles,
-} from '@material-ui/core';
+import { AppBar, Typography, Link } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() =>
   createStyles({
     appBar: {
       top: 'auto',
       bottom: 0,
+      display: 'flex'
+    },
+    title: {
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    link: {
+      justifyContent: 'center'
     },
   }),
 );
 const Footer = () => {
   const classes = useStyles();
   return (
-    <AppBar position="fixed" color="secondary" className={classes.appBar}>
-      <Typography>Footer</Typography>
+    <AppBar position="fixed" color="transparent" className={classes.appBar}>
+      <Typography variant="subtitle1" className={classes.title}>
+        <Link
+          href="https://iexcloud.io"
+          target="_blank"
+          rel="noopener"
+          color="textPrimary"
+          underline="none"
+          className={classes.link}
+        >
+          Data provided by IEX Cloud
+        </Link>
+      </Typography>
     </AppBar>
   );
 };
