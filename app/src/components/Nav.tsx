d07a8280ebde98b1a47463c8db844d43852cb37d
Bryan Grill <grillb@gmail.com>
2020-06-07 07:45:36 -0400

    build out nav

--- app/src/components/Nav.tsx
+++ app/src/components/Nav.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Link,
+  Button,
+  IconButton,
+} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Menu as MenuIcon } from '@material-ui/icons';
+
+import { Link as RRLink } from 'react-router-dom';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+    link: {},
+  }),
+);
 
 const Nav = () => {
+  const classes = useStyles();
   return (
-    <AppBar position="fixed" color="primary">
-      <Typography>nav</Typography>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <IconButton aria-label="delete" disabled>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link
+              component={RRLink}
+              to="/"
+              color="textPrimary"
+              underline="none"
+            >
+              Stock Quotes
+            </Link>
+          </Typography>
+          <Button color="inherit" disabled>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
