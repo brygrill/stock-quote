1910955e527cb85696a3b4ca33fc512d18e31ed8
Bryan Grill <grillb@gmail.com>
2020-06-08 18:00:55 -0400

    start card highlight layout

--- app/src/components/HighlightCard.tsx
+++ app/src/components/HighlightCard.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+  useMediaQuery,
+} from '@material-ui/core';
+
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+import { breakpoint } from '../config';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    stats: {
+      display: 'flex',
+
+    },
+    statsDesktop: {
+      display: 'flex',
+      flexDirection: 'column',
+      padding: '0 0.5em'
+    },
+  }),
+);
 
 const HighlightCardWrapper = ({ title }: { title: string }) => {
+  const classes = useStyles();
+  const isDesktop = useMediaQuery(breakpoint);
   return (
-    <Card>
+    <Card variant="outlined">
       <CardContent>
-        <Typography>{title}</Typography>
+        <Typography variant="h4" component="h2">
+          {title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          className={isDesktop ? classes.statsDesktop : classes.stats}
+        >
+          <span>$100.69</span>
+          <span>+ $2.21</span>
+          <span>+ 0.78%</span>
+        </Typography>
       </CardContent>
+      <CardActions>
+        <Button size="small" color="secondary">
+          More
+        </Button>
+      </CardActions>
     </Card>
   );
 };
