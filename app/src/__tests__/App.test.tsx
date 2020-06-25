68bf8310ef0806286e9b006eb68248d189b645b1
Bryan Grill <grillb@gmail.com>
2020-06-25 06:02:10 -0400

    fix test

--- app/src/__tests__/App.test.tsx
+++ app/src/__tests__/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('it works', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/QQQ/i);
   expect(linkElement).toBeInTheDocument();
 });
